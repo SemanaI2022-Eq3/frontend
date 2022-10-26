1f2659c4089e6ffde1d032ab37073d07c8d36ab8
Rodrigo Casale <popobasura3@hotmail.com>
2022-10-26 10:10:00 -0500

    Header and routing added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+
+import { CssVarsProvider } from "@mui/joy/styles";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
+import { TextField, IconButton } from "@mui/joy";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+
+import Header from "./components/Header";
+
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Teacher from "./components/Teacher";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CssVarsProvider>
+      <Header>
+        <Typography component="h1" fontWeight="xl">
+          Files
+        </Typography>
+
+
+        <TextField
+          size="sm"
+          placeholder="Buscar profesores"
+          startDecorator={<SearchRoundedIcon color="primary" />}
+          endDecorator={
+            <IconButton variant="outlined" size="sm" color="neutral">
+              <Typography
+                fontWeight="lg"
+                fontSize="sm"
+                textColor="text.tertiary"
+              >
+                /
+              </Typography>
+            </IconButton>
+          }
+          sx={{
+            flexBasis: "500px",
+            display: {
+              xs: "none",
+              sm: "flex",
+            },
+          }}
+        />
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/teacher" element={<Teacher />} />
+      </Routes>
+    </CssVarsProvider>
   );
 }
 
